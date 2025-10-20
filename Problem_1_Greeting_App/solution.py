90d584d3136bb3493a0d880fdd4edc5c58936590
dbu150 <yoniy5006@gmail.com>
2025-10-20 20:18:49 +0300

    Update solution.py

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,1 +1,14 @@
-# your code here
+me =input("what  is your name?")
+favorite_food=input("what is your  favorite food?")
+current_year=int(input("what is the current year?"))
+age=int(input("what is your age"))
+#caculate the the birth_years
+birth_year=current_year - age
+print(f"hello  {name}")
+print(f"your favorite is {favorite_food}")
+print(f"your birth year is {birth_year}")
+                       
+
+
+
+
