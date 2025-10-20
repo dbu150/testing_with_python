146e48bfffc0b3ed5a495451c7bb974b1f0f8f71
dbu150 <yoniy5006@gmail.com>
2025-10-20 20:37:48 +0300

    Update solution.py

--- Problem_2_Unit_Converter/solution.py
+++ Problem_2_Unit_Converter/solution.py
@@ -1,1 +1,8 @@
 # your code here
+                       
+#this  the conversion unit of the temperture
+celsius=int(input("enter yhe temepratuere in celsius "))
+fahrenheit=(9/5*celsius)+32 #CONVERt CELSIUS TO fahreheit
+print(f"termperature  in fahaniet={fahrenheit}")
+
+                 
