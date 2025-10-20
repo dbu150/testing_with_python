4765abccea50e2e05cbed434e994b74dd74cede0
dbu150 <yoniy5006@gmail.com>
2025-10-20 21:22:45 +0300

    Update solution.py

--- Problem_3_Simple_Calculator/solution.py
+++ Problem_3_Simple_Calculator/solution.py
@@ -1,1 +1,23 @@
-# your code here
+# your code her
+# prforme addition,substraction,multiplication,devision user input
+num1=int(input("enter number 1"))
+num2=int(input("enter numebr 2"))
+oper=(input("enter the operation"))
+match oper:
+  case "+":
+    sum=num1 +num2
+    print(f"the sum of two number is {sum}")
+  case "-":
+    sub=num1-num2
+    print(f"the sustraction of two number is {sub}")
+  case "*":
+    mul=num1*num2
+    print(f"the multplication of two numbers is {mul}")
+  case "/" :
+    if num2!=0:
+      div=num1/num2
+      print(f"the division of two number is {div}")
+  case _:
+
+   print("display in valid opration")
+  
