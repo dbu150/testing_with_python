d84e3cfeab46efec242628dfee8d4db08a59d5ef
dbu150 <yoniy5006@gmail.com>
2025-10-22 09:44:39 -0700

    Update conditional_logic_exercises.py

--- worksheets/conditional_logic_exercises.py
+++ worksheets/conditional_logic_exercises.py
@@ -1,2 +1,14 @@
 print("Conditional Logic Exercises")
 # Practice if/elif/else statements here.
+a=int(input("enter the number "))
+b=int(input("enter the number "))
+c=int(input("enter the number"))
+if a > b:
+  if a > c:
+    print("the lager number is" ,a)
+  else:
+    print("the lager number is" ,c)
+elif b > c:
+  print("the lager number is" ,b)
+else:
+  print("the lager number is "  ,c) 
