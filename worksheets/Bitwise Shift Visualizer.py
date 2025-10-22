a04e6b4afa695fc9d704d4f87edfa4d6c6f55b54
dbu150 <yoniy5006@gmail.com>
2025-10-22 03:41:43 -0700

    Implement bitwise shift visualizer with user input
    Added functionality to input an integer and shift amount, then display the results of left and right bitwise shifts along with their binary representations.

--- worksheets/Bitwise Shift Visualizer.py
+++ worksheets/Bitwise Shift Visualizer.py
@@ -5,3 +5,19 @@ Show results of left shift and right shift.
 
 Print the binary form before and after.
 """
+integer=int(input("enter the integer number")) #this will be shifted number
+
+snumber =int(input("ente shift number"))
+result1 =integer << snumber #caculate the left shift number
+print("this left shift=",result1)
+result2= integer >> snumber #calculate the right shiift
+print("this right shift=",result2)
+# binart before 
+binary_before=bin(integer)[2:] #before the shift of the number
+
+print( "this is the binary of before left and right shift the integer number is ",binary_before)
+#the binary number of after the shift
+print("the binary of left shift integer=",bin(result1)[2:])
+print("the binary of right shift integer=",bin(result2)[2:])
+
+
